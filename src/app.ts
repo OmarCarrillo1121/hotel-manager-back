3da27b9aa09f047dfa44cf7404becf11869ec1c8
OmarCarrillo1121 <omarcarrillo1121@gmail.com>
2024-02-06 14:38:34 -0600

    Added middlewares

--- src/app.ts
+++ src/app.ts
@@ -1,8 +1,14 @@
 import express, { Application } from "express";
 import routes from "./routes/index";
+import cors from "cors";
 
 const app: Application = express();
 
+// MIDDLEWARES
+app.use(cors());
+app.use(express.json());
+app.use(express.static("public"));
+// ROUTES
 app.use("/", routes);
 
 export default app;
--- src/routes/index.ts
+++ src/routes/index.ts
@@ -3,9 +3,6 @@ import { Router } from "express";
 const router = Router();
 import registerRouter from "./register";
 
-// Configurar los routers
-// Ejemplo: router.use('/auth', authRouter);
-
 router.use("/register", registerRouter);
 
 export default router;
